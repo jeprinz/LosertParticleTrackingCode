9b34c05337574151763de1f5012c681a308f8d0e
jacob5555555555 <jacobeliasprinz@gmail.com>
2018-02-13 22:52:06 -0500

    Factored a couple other things out into functinos

--- analyze_scan_orientations_hdf5.m
+++ analyze_scan_orientations_hdf5.m
@@ -1,6 +1,5 @@
 function out_as=analyze_scan_orientations_hdf5(imagefolder,scan_number,start_image,end_image,x1,x2,y1,y2,radius,sigma0,AR_z)
 tic
-splits = 5;
 %KERNEL Radius is larger than physical radius
 no_images=end_image-start_image+1;
 
@@ -20,11 +19,9 @@ IMS=load_images_hdf5(start_image,end_image,x1,x2,y1,y2,imagefolder,scan_number);
 disp('a_s: Thresholding and inverting images');
 IMS = thresh_invert(IMS);
 %%
-%Test bandpass to get size
-%bpass-jhw cuts off radius*2 from EACH side in x
-%so the x and y dimensions are reduced by !!4*radius!!
+
 disp('a_s: Band-filtering all images');
-IMSbp=bandpass(radius,IMS);
+IMSbp=bandpass(radius,IMS); %remember bandpass cuts off radius*2 from EACH side in x so the x and y dimensions are reduced by !!4*radius!!
 
 %% create IMSCr (thresholded bandpassed image)
 %TWEAK THRESHOLD
@@ -33,62 +30,29 @@ thresh_val=1e-5;
 
 IMSCr = IMSbp <= thresh_val;
 
-% IMSCr=max(max(max(IMSbp(:,:,:))))-IMSbp;
-% for k=1:length(IMSCr(1,1,:))
-%    IMSCr(:,:,k)=double(im2bw(IMSCr(:,:,k),thres_val));%thresholding value may change for different frames
-% end
-
-%%
 %Convolve
 disp('a_s: Convolving... This may take a while');
-Convol=single(jcorr3d(IMSbp,Gauss_sph,splits));
+Convol = convolve(IMSbp, Gauss_sph);
 
-sizekernel=size(Gauss_sph);
-sC=size(Convol);%convolve does change size by adding a Kernel radius on either side of all dimmensions
-Convol=Convol( round(sizekernel(1)/2) : round(sC(1) - sizekernel(1)/2) ...
-            ,  round(sizekernel(2)/2) : round(sC(2) - sizekernel(2)/2) ...
-            ,  round(sizekernel(3)/2) : round(sC(3) - sizekernel(3)/2)  );%crops (radius of the kernel*2) in order to bring Covol back to the same dimensions as the bandpassed image
-
-sIMSCr=size(IMSCr); 
-sC=size(Convol);
-if sIMSCr~=sC
-    disp('error: size of bandpassed image ~= to convolved+cropped image')
-    return
-end
 %% create pkswb (thresholded convolution image)
 disp('a_s: Thresholding');
 %TWEAK THRESHOLD
 disp('using new threshold method');
-pksbw = Convol > 0;
-% for k=1:size(Convol,3)
-%     pksbw(:,:,k)=im2bw(double(Convol(:,:,k)),0.99);%thresholding only works on 2d arrays, may have to adjust
-% end
+IMSthresholded = Convol > 0;
 %%
 disp('a_s: Tagging regions');
-L=bwlabeln(pksbw);%output is an array with size of pksbw where all touching pixels in the 3d array have the same id number, an integer)
-disp('a_s: Imposing Volume minimum of 4');
-Resultunf=regionprops(L,'Area');%[NOTE L is array of TAGGED regions]; creates structure Resultunf with one 1x1 matricies(in a col) that are the areas of the tagged regions (sequentially by tag #) 
-idx=find([Resultunf.Area]>=4);%index of all regions with nonzero area
-L2=ismember(L,idx);%output is array with size L of 1's where elements of L are in the set idx~which is just 1:number of regions. Therefore it converts all tagged regions to all 1's
-L3=bwlabeln(L2);% L3 now retaggs (L3=old L2)
-%%
-disp('a_s: Determining weighted centroid locations and orientations');
-s=regionprops(L3,'PixelIdxList', 'PixelList');%s is a struct that holds structs for each tagged 
-%region. the 2nd level struct holds two matricies: pxlidlist is the linear indicies
-%of the nonzero pxls in that region. pxllist is the
-%coordinates of each pxl in that region. NOTE:these indicies apply to the
-%bandpassed image
+region_list = label_regions(IMSthresholded);
 %% 
 % The next step: try adjusting center-finding method
 Cr = 2*radius; %the amount that gets cropped off both sides earlier by bandpass
-Result=zeros(numel(s),11);
-for k = 1:numel(s)%#elements in s (#regions or particles)
-    idx = s(k).PixelIdxList;%lin index of all points in region k
+Result=zeros(numel(region_list),11);
+for k = 1:numel(region_list)%#elements in s (#regions or particles)
+    idx = region_list(k).PixelIdxList;%lin index of all points in region k
     pixel_values = double(Convol(idx)+.0001);%list of values of the pixels in convol which has size of idx
     sum_pixel_values = sum(pixel_values);   
-    x = s(k).PixelList(:, 1);%the list of x-coords of all points in the region k WITH RESPECT TO the bandpassed image
-    y = s(k).PixelList(:, 2);
-    z = s(k).PixelList(:, 3);
+    x = region_list(k).PixelList(:, 1);%the list of x-coords of all points in the region k WITH RESPECT TO the bandpassed image
+    y = region_list(k).PixelList(:, 2);
+    z = region_list(k).PixelList(:, 3);
     xbar = sum(x .* pixel_values)/sum_pixel_values + Cr;%PLUS Cr BECAUSE
     ybar = sum(y .* pixel_values)/sum_pixel_values + Cr;%I CUT OFF Cr OF THE IMAGE DURING BANDPASS!(in x and y only) AND cropped kernelradius/2 off each side (in x,y,z)(but it was put back)                                                         %cropped radius/2 of each side
     zbar = sum(z .* pixel_values)/sum_pixel_values;%adding Cr above brings back to IMS
@@ -149,7 +113,7 @@ for k = 1:numel(s)%#elements in s (#regions or particles)
  %disp(num2str(k))
     if max(max(isnan(Covmat)))==1;%checks if any element of covmat is NAN==1
       
-         Result(k,9:11)=NaN(1,3);
+         Result(k,9:11)=NaN(1,3);sum
         continue
     else
         [V,D] = eig(Covmat);
@@ -182,7 +146,10 @@ disp('***********************************************');
 toc
 out_as=Result(2:length(Result),:);
 
+%% The folowing functions are helpers to the above, which do the individual processing steps
+
 function out=bandpass(radius,IMS)
+%Note that the bandpass will crop off 2*radius from sides of image
 
 s = size(IMS);
 no_images = s(3);
@@ -196,3 +163,34 @@ end
 
 out=IMSbp;
 
+
+function out=convolve(IMSbp, Gauss_sph)
+splits = 5; %has to do with RAM constraints on jcorr3d function
+
+Convol=single(jcorr3d(IMSbp,Gauss_sph,splits));
+
+sizekernel=size(Gauss_sph);
+sC=size(Convol);%convolve does change size by adding a Kernel radius on either side of all dimmensions
+Convol=Convol( round(sizekernel(1)/2) : round(sC(1) - sizekernel(1)/2) ...
+            ,  round(sizekernel(2)/2) : round(sC(2) - sizekernel(2)/2) ...
+            ,  round(sizekernel(3)/2) : round(sC(3) - sizekernel(3)/2)  );%crops (radius of the kernel*2) in order to bring Covol back to the same dimensions as the bandpassed image
+
+sIMSCr=size(IMSbp); 
+sC=size(Convol);
+if sIMSCr~=sC
+    disp('error: size of bandpassed image ~= to convolved+cropped image')
+    return
+end
+out = Convol;
+
+function out = label_regions(IMSthresholded)
+L=bwlabeln(IMSthresholded);%output is an array with size of IMSthresholded where all touching pixels in the 3d array have the same id number, an integer)
+disp('a_s: Imposing Volume minimum of 4');
+Resultunf=regionprops(L,'Area');%[NOTE L is array of TAGGED regions]; creates structure Resultunf with one 1x1 matricies(in a col) that are the areas of the tagged regions (sequentially by tag #) 
+idx=find([Resultunf.Area]>=4);%index of all regions with nonzero area
+L2=ismember(L,idx);%output is array with size L of 1's where elements of L are in the set idx~which is just 1:number of regions. Therefore it converts all tagged regions to all 1's
+L3=bwlabeln(L2);% L3 now retaggs (L3=old L2)
+%%
+disp('a_s: Determining weighted centroid locations and orientations');
+region_list=regionprops(L3,'PixelIdxList', 'PixelList');%s is a struct that holds structs for each tagged 
+out = region_list;
